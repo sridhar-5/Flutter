55a78525f851bc51de74a13b3e1f7255728b8421
sridhar-5 <kv.sridharsai@gmail.com>
2021-06-10 01:23:09 +0530

    second commit

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.deepOrange,
-      appBar: AppBar(
-        title: Text("This is my first Flutter App"
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "This is my first Flutter app",
+            style: TextStyle(
+                color: Colors.cyan,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 5),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
         ),
-        backgroundColor: Colors.blue,
+        body: Center(
+          child: Text(
+            'This is the body of the trail application',
+            style: TextStyle(
+                color: Colors.amber,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 6,
+                fontSize: 30),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('Try!'),
+        ),
       ),
-      body: Center(child: Text("This is the body"
-      ),),
-    )
-  ));
-}
+    ));
